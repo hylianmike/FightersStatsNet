97a11128335d62f1d5f1e1c361ee887f0c7f6e92
Michael Rosanelli <michaelrosanelli105@gmail.com>
2022-11-23 11:17:45 -0500

    add apis for games, fighters and attacks

---
+++ FightersStatsNet/Controllers/api/AttacksController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FightersStatsNet.Data;
+using FightersStatsNet.Models;
+
+namespace FightersStatsNet.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttacksController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttacksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Attacks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Attack>>> GetAttacks()
+        {
+            return await _context.Attacks.ToListAsync();
+        }
+
+        // GET: api/Attacks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Attack>> GetAttack(int id)
+        {
+            var attack = await _context.Attacks.FindAsync(id);
+
+            if (attack == null)
+            {
+                return NotFound();
+            }
+
+            return attack;
+        }
+
+        // PUT: api/Attacks/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAttack(int id, Attack attack)
+        {
+            if (id != attack.AttackId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(attack).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AttackExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Attacks
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Attack>> PostAttack(Attack attack)
+        {
+            _context.Attacks.Add(attack);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAttack", new { id = attack.AttackId }, attack);
+        }
+
+        // DELETE: api/Attacks/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAttack(int id)
+        {
+            var attack = await _context.Attacks.FindAsync(id);
+            if (attack == null)
+            {
+                return NotFound();
+            }
+
+            _context.Attacks.Remove(attack);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AttackExists(int id)
+        {
+            return _context.Attacks.Any(e => e.AttackId == id);
+        }
+    }
+}
---
+++ FightersStatsNet/Controllers/api/FightersController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FightersStatsNet.Data;
+using FightersStatsNet.Models;
+
+namespace FightersStatsNet.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FightersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FightersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Fighters
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Fighter>>> GetFighters()
+        {
+            return await _context.Fighters.ToListAsync();
+        }
+
+        // GET: api/Fighters/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Fighter>> GetFighter(int id)
+        {
+            var fighter = await _context.Fighters.FindAsync(id);
+
+            if (fighter == null)
+            {
+                return NotFound();
+            }
+
+            return fighter;
+        }
+
+        // PUT: api/Fighters/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFighter(int id, Fighter fighter)
+        {
+            if (id != fighter.FighterId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(fighter).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FighterExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Fighters
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Fighter>> PostFighter(Fighter fighter)
+        {
+            _context.Fighters.Add(fighter);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFighter", new { id = fighter.FighterId }, fighter);
+        }
+
+        // DELETE: api/Fighters/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFighter(int id)
+        {
+            var fighter = await _context.Fighters.FindAsync(id);
+            if (fighter == null)
+            {
+                return NotFound();
+            }
+
+            _context.Fighters.Remove(fighter);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FighterExists(int id)
+        {
+            return _context.Fighters.Any(e => e.FighterId == id);
+        }
+    }
+}
